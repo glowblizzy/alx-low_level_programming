d6cf2131caf94c32264c63d71a7fc5adae61c8b2
glowblizzy <eveshoyanblessing101@gmail.com>
2022-10-17 14:51:09 -0700

    My seventh task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,10 +14,8 @@ int main(void)
 			putchar((num % 10) + '0');
 
 		for (letter = 'a'; letter <= 'f'; letter++);
-		{
-			putchar(letter):
-		}	
-		putchar('\n');
+		putchar(letter):	
+		putchar('\n'):
 
 		return (0);
 }
