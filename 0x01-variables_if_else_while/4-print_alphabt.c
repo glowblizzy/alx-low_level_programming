5a95ff1f63ba7fe9aa67b7a4a68ff63598e50e12
glowblizzy <eveshoyanblessing101@gmail.com>
2022-10-17 13:17:10 -0700

    My forth task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- * Description: prints lowercase alphabets
- * Return: Always 0 (success)
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
 */
 int main(void)
 {
-		int ch;
+	char letter;
 
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-				if (ch != 'e' && ch != 'q')
-				{
-					putchar(ch);													}															else
-				{															}													}		
-		putchar('\n');
-		return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+			putchar('\n');
+			return (0);
 }
