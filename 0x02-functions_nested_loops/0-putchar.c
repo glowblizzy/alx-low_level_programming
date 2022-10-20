691666f89a2024f89b20a92619b3d683468126c0
glowblizzy <eveshoyanblessing101@gmail.com>
2022-10-19 22:48:59 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- *  * main - Print _putchar
- *   * Return: Always 0
- *    */
+ * (void)c
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
-{
+ {
 		_putchar('_');
 		_putchar('p');
 		_putchar('u');
@@ -16,4 +17,4 @@ int main(void)
 		_putchar('r');
 		_putchar('\n');
 		return (0);
-}
+ }
